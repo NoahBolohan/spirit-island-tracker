46ebff10684b1bcd6fce82d673fc99d3a0bd4a8b
Noah Bolohan <noah.bolohan@gmail.com>
2025-07-15 07:06:04 -0400

    score computer done (close #2)

--- js/settings.js
+++ js/settings.js
@@ -224,10 +224,162 @@ $(document).ready(
                         }
                     );
                 });
+
+                reset_score();
                 
                 switch_theme(
                     "default"
                 );
+            }
+        );
+    }
+)
+
+$(document).ready(
+    function() {
+
+        $("#button_compute_score").on(
+            "click",
+            function() {
+                $("#modal_compute_score").modal("show");
+            }
+        );
+    }
+)
+
+$(document).ready(
+    function() {
+
+        $("#button_close_modal_compute_score").on(
+            "click",
+            function() {
+                $("#modal_compute_score").modal("hide");
+            }
+        );
+    }
+)
+
+// Score calculator
+function score_calculator() {
+
+    var total_score = 0;
+
+    // if ($("#col_select_scenario").val() != "Second Wave") {
+        
+        // Score-checking based on victory
+        if ($("#checkbox_win").is(":checked")) {
+            
+            // Add score for difficulty
+            total_score += 5*$("#col_difficulty_number").text();
+
+            // Add score for winning
+            total_score += 10;
+
+            // Add score for remaining invader cards in the deck
+            total_score += 2*$("#text_invader_cards_in_deck").val();
+        }
+        else {
+
+            // Add score for difficulty
+            total_score += 2*$("#col_difficulty_number").text();
+
+            // Add score for remaining invader cards in the deck
+            total_score += 1*$("#text_invader_cards_not_in_deck").val();
+        }
+
+        // Add score for remaining Dahan
+        total_score += Math.floor($("#text_remaining_dahan").val()/$("#text_n_players").val());
+
+        // Remove score for blight on the island
+        total_score -= Math.floor($("#text_blight_on_island").val()/$("#text_n_players").val());
+
+        // Assign the score value to the score display div
+        $("#col_score_number").text(
+            total_score
+        )
+    // }
+}
+
+// Set an event listener for computing score on n playuers change
+$(document).ready(
+    function() {
+        $("#text_n_players").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+// Set an event listener for computing score on victory change
+$(document).ready(
+    function() {
+        $("#checkbox_win").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+// Set an event listener for computing score on invader cards in deck change
+$(document).ready(
+    function() {
+        $("#text_invader_cards_in_deck").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+// Set an event listener for computing score on invader cards not in deck change
+$(document).ready(
+    function() {
+        $("#text_invader_cards_not_in_deck").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+// Set an event listener for computing score on remaining Dahan change
+$(document).ready(
+    function() {
+        $("#text_remaining_dahan").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+// Set an event listener for computing score on blight on island change
+$(document).ready(
+    function() {
+        $("#text_blight_on_island").on(
+            "change",
+            score_calculator
+        )
+    }
+)
+
+function reset_score() {
+    $("#text_n_players").val("");
+    $("#checkbox_win").attr("checked", false);
+    $("#text_invader_cards_in_deck").val("");
+    $("#text_invader_cards_not_in_deck").val("");
+    $("#text_remaining_dahan").val("");
+    $("#text_blight_on_island").val("");
+    
+    $("#col_score_number").text(
+        0
+    )
+}
+
+$(document).ready(
+    function() {
+
+        $("#button_reset_score").on(
+            "click",
+            function() {
+                reset_score();
             }
         );
     }
