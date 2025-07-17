0a391419a9603366a8014367038950cc12e220f9
Noah Bolohan <noah.bolohan@gmail.com>
2025-07-17 10:42:06 -0400

    About modal and some button positioning

--- js/settings.js
+++ js/settings.js
@@ -515,3 +515,27 @@ $(document).ready(
         )
     }
 )
+
+$(document).ready(
+    function() {
+
+        $("#button_about").on(
+            "click",
+            function() {
+                $("#modal_about").modal("show");
+            }
+        );
+    }
+)
+
+$(document).ready(
+    function() {
+
+        $("#button_close_modal_about").on(
+            "click",
+            function() {
+                $("#modal_about").modal("hide");
+            }
+        );
+    }
+)
